9f34a18e6238362a7aa36608855ab2d0ff1ec65b
rooide0000 <rooide0000@gmail.com>
2023-02-03 15:18:06 +0900

    Let's go

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
 import sys
-sys.path.append('C:/Users/rooid/OneDrive - 대구광역시교육청/code/ATC-Training/build')
+sys.path.append('C:/Users/rooid/OneDrive - 대구광역시교육청/code/ATC-Training/git/ATC-Training/Build')
  
 import gui
